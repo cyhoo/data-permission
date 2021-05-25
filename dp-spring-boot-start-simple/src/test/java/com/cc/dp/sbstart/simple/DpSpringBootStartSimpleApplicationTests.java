6bc4cd94855d1f5a3f8a36c42b1632f8dc4ee2f7
Cai Weihao <caiweihao@shein.com>
2021-05-25 15:56:29 +0800

    add

---
+++ dp-spring-boot-start-simple/src/test/java/com/cc/dp/sbstart/simple/DpSpringBootStartSimpleApplicationTests.java
@@ -0,0 +1,12 @@
+package com.cc.dp.sbstart.simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSpringBootStartSimpleApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
